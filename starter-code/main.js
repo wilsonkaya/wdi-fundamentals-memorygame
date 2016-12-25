f2684447285f3a973d4b068ad5f236e558469fe2
wilsonKaya <fatihkaya2@hotmail.com>
2016-12-25 12:16:58 -0500

    function added

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+
+function createCards(x){
+	var cardNumbers  = x;
+	for (var i = 0; i < cardNumbers; i++){
+	var newCard = document.createElement('div');
+	newCard.className = "card"
+	// document.getElementById('game-board')[i].appendChild(newCard)
+	document.getElementsByTagName('div')[0].appendChild(newCard)
+}
+
+
+}
+createCards(4)
